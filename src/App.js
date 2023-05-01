8498c24dc09548a92cc06fdf58ed140cb73272ed
Phinjock Sherpa <phinjock@gmail.com>
2023-05-01 14:52:14 -0700

    defintion included

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        React theory
+        <ol>
+          <li>What is a component in react?</li>- A component is a small and
+          reusable piece of code that represents a part of the user
+          interface(UI). It can contain any combination of HTML,CSS and JS and
+          is easily able to be inserted into a bigger webpage.
+          <li>What is JSX in react</li>- Javascript Syntax extention is an
+          extention that allows us to write HTML like codes directly in our JS
+          files. This makes the codes simpler to read as we do not have to go
+          back and forth between two files.
+        </ol>
       </header>
     </div>
   );
