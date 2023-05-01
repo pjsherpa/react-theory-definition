97cad3d9f92dae084d5ca6463be54a4db35e9581
Phinjock Sherpa <phinjock@gmail.com>
2023-05-01 15:15:12 -0700

    add definition for state

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        React theory
+      <div className="App-header">
+        <header>React theory</header>
         <ol>
           <li>What is a component in react?</li>- A component is a small and
           reusable piece of code that represents a part of the user
@@ -14,8 +14,15 @@ function App() {
           extention that allows us to write HTML like codes directly in our JS
           files. This makes the codes simpler to read as we do not have to go
           back and forth between two files.
+          <li>What is State in react?</li>- In react a state is an object that
+          stores data and can change over time and affects the behaviour of a
+          component and its rendering. It is essentially a way for a component
+          to keep track of its own data and update their apperance and behaviour
+          accordingly. Using state allows our react component to be dynamic by
+          allowing users to give input and change component over time.
+          <li>What is Props?</li>
         </ol>
-      </header>
+      </div>
     </div>
   );
 }
