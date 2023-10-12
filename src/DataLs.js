1e5a41f63850a7e8475cc66de1dfac455226a04b
Phinjock Sherpa <phinjock@gmail.com>
2023-10-12 10:52:53 -0700

    updated table

---
+++ src/DataLs.js
@@ -0,0 +1,10 @@
+const dataLs = {
+  list: [
+    { name: "Peter", age: 22, score: 150 },
+    { name: "Dimitri", age: 24, score: 120 },
+    { name: "Timmy", age: 35, score: 110 },
+    { name: "Joe", age: 32, score: 160 },
+  ],
+};
+
+export default dataLs;
--- src/component/LeaderBoard.js
+++ src/component/LeaderBoard.js
@@ -1,19 +1,41 @@
-import React from "react";
-
+import React, { useState } from "react";
+import dataLs from "../DataLs";
 const LeaderBoard = () => {
+  const [player, setPlayer] = useState(dataLs.list);
+  const ascHandler = (item) => {
+    const asc = [...player].sort((a, b) => a[item] - b[item]);
+    setPlayer(asc);
+  };
+
+  const ascNameHandler = () => {
+    const asc = [...player].sort((a, b) => {
+      if (a.name < b.name) {
+        return -1;
+      } else if (a.name > b.name) {
+        return 1;
+      } else {
+        return 0;
+      }
+    });
+    setPlayer(asc);
+  };
+
   return (
     <div>
       <h1>LeaderBoard</h1>
+
       <tr>
-        <th>Name</th>
-        <th>Age</th>
-        <th>Score</th>
+        <th onClick={() => ascNameHandler("name")}>Name</th>
+        <th onClick={() => ascHandler("age")}>Age</th>
+        <th onClick={() => ascHandler("score")}>Score</th>
       </tr>
-      <tr>
-        <td>Name</td>
-        <td>Age</td>
-        <td>Score</td>
-      </tr>
+      {player.map((players, index) => (
+        <tr key={index}>
+          <td>{players.name}</td>
+          <td>{players.age}</td>
+          <td>{players.score}</td>
+        </tr>
+      ))}
     </div>
   );
 };
